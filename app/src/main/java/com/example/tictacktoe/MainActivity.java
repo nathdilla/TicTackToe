0912e388085d6c0057b272542296fd9679572d1a
rjedwards105 <rjedwards105@gmail.com>
2020-11-10 02:53:36 -0800

    Created GameActivity  - This has the 9 buttons for the tic tac toe board  - Score  - Back Button that leads back to main menu
    Created InfoActivity
     - This teaches user how to play the game
     - Added a back button tha leads back to main menu
    
    Updated MainActivity (The landing page)
     - Leads to GameActivity (the game)
     - Leads to InfoActivity (the instructions)

--- app/src/main/java/com/example/tictacktoe/MainActivity.java
+++ app/src/main/java/com/example/tictacktoe/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.tictacktoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gameButton;
+    private Button infoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameButton = (Button) findViewById(R.id.gameButton);
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameActivty();
+            }
+        });
+
+        infoButton = (Button) findViewById(R.id.infoButton);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfoActivity();
+            }
+        });
+
+
+
+    }
+
+    public void openGameActivty() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInfoActivity() {
+        Intent intent2 = new Intent(this, InfoActivity.class);
+        startActivity(intent2);
     }
 }
